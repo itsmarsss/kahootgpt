10ddef4c93c6256bcc7c478c1779e938b1f613c2
itsmarsss <itsmarzzzzzz@gmail.com>
2023-03-29 23:02:56 -0400

    Add status label

--- popup.js
+++ popup.js
@@ -1,6 +1,7 @@
 const checkbox = document.getElementById("checkbox");
 const toggle = document.getElementById("toggle");
 const powericon = document.getElementById("power-icon");
+const tapstatus = document.getElementById("autoclick-status");
 
 let toggled = false;
 
@@ -9,10 +10,14 @@ checkbox.addEventListener("click", function () {
         checkbox.style.boxShadow = "0 4px 4px -2px #000";
         toggle.style.background = "#525252";
         powericon.style.fill = "#b7b7b7";
+        tapstatus.innerHTML = "Auto-tap is OFF";
+        tapstatus.style.color = "#b7b7b7";
     } else {
         checkbox.style.boxShadow = "none";
         toggle.style.background = "#9d86c3";
         powericon.style.fill = "#9d86c3";
+        tapstatus.innerHTML = "Auto-tap is ON";
+        tapstatus.style.color = "#864cbf";
     }
     toggled = !toggled;
 });
