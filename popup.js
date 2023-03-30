e238fc15af8086a94b9f82b62d36853ff33da7f2
itsmarsss <itsmarzzzzzz@gmail.com>
2023-03-29 22:50:20 -0400

    Add toggle animation

--- popup.js
+++ popup.js
@@ -1,11 +1,18 @@
 const checkbox = document.getElementById("checkbox");
-const toggle = document.getElementsById("toggle");
-const powericon = document.getElementsById("power-icon");
+const toggle = document.getElementById("toggle");
+const powericon = document.getElementById("power-icon");
 
 let toggled = false;
 
 checkbox.addEventListener("click", function () {
     if (toggled) {
+        checkbox.style.boxShadow = "0 4px 4px -2px #000";
+        toggle.style.background = "#525252";
+        powericon.style.fill = "#b7b7b7";
+    } else {
+        checkbox.style.boxShadow = "none";
         toggle.style.background = "#9d86c3";
+        powericon.style.fill = "#9d86c3";
     }
+    toggled = !toggled;
 });
