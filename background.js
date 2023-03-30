e338b1bcbfbf8edb5734f0c24888482fa290e042
itsmarsss <itsmarzzzzzz@gmail.com>
2023-03-29 22:50:47 -0400

    Add events

--- background.js
+++ background.js
@@ -2,9 +2,16 @@ chrome.tabs.onUpdated.addListener((tabId, tab) => {
     if (tab.url && tab.url.includes("kahoot.it")) {
         // Do stuff
 
+        // Only get answer
         chrome.tabs.sendMessage(tabId, {
-            type: "new_question",
+            type: "ANS",
             question: "question"
-        })
+        });
+
+        // Get answer and best match
+        chrome.tabs.sendMessage(tabId, {
+            type: "BST",
+            question: "question"
+        });
     }
 })
