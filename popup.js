ad09bb4f055b3938dd0519e34ff51ceb0c5ce04d
itsmarsss <itsmarzzzzzz@gmail.com>
2023-04-24 20:28:40 -0400

    Fix display text

--- popup.js
+++ popup.js
@@ -128,13 +128,13 @@ function toggleAutoTap() {
         checkbox.style.boxShadow = "0 4px 4px -2px #000";
         toggle.style.background = "#525252";
         powericon.style.fill = "#b7b7b7";
-        tapstatus.innerHTML = "Auto-tap is OFF";
+        tapstatus.innerHTML = "Auto-tap OFF";
         tapstatus.style.color = "#b7b7b7";
     } else {
         checkbox.style.boxShadow = "none";
         toggle.style.background = "#9d86c3";
         powericon.style.fill = "#9d86c3";
-        tapstatus.innerHTML = "Auto-tap is ON";
+        tapstatus.innerHTML = "Auto-tap ON";
         tapstatus.style.color = "#864cbf";
     }
     toggled = !toggled;
