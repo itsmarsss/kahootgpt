e47892f0fa3fdc2d7c1c3470537fc0e2d512e6e9
itsmarsss <88464166+itsmarsss@users.noreply.github.com>
2026-02-05 11:55:29 -0500

    Refactor Home component and update routing

--- docs/src/main.tsx
+++ docs/src/main.tsx
@@ -6,7 +6,7 @@ import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/kahootgpt">
       <App />
     </BrowserRouter>
   </StrictMode>,
