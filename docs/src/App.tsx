9389d27188a7df4e1aac9f920118846b04f839e1
itsmarsss <88464166+itsmarsss@users.noreply.github.com>
2026-02-06 15:39:11 -0500

    Add auto scroll to top

--- docs/src/App.tsx
+++ docs/src/App.tsx
@@ -1,4 +1,5 @@
 import { Routes, Route } from 'react-router-dom';
+import ScrollToTop from './ScrollToTop';
 import Home from './Home';
 import Tutorial from './Tutorial';
 import Privacy from './Privacy';
@@ -8,13 +9,16 @@ import './App.css';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/tutorial" element={<Tutorial />} />
-      <Route path="/privacy" element={<Privacy />} />
-      <Route path="/terms" element={<Terms />} />
-      <Route path="/license" element={<License />} />
-    </Routes>
+    <>
+      <ScrollToTop />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/tutorial" element={<Tutorial />} />
+        <Route path="/privacy" element={<Privacy />} />
+        <Route path="/terms" element={<Terms />} />
+        <Route path="/license" element={<License />} />
+      </Routes>
+    </>
   );
 }
 
