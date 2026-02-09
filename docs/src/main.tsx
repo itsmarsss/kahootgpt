bfacab9950eba01382f1e13fe96f4e3a76229360
itsmarsss <88464166+itsmarsss@users.noreply.github.com>
2026-02-09 15:43:13 -0500

    Use Hash router to handle routes client side

--- docs/src/main.tsx
+++ docs/src/main.tsx
@@ -1,15 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.tsx'
 
-const basename = import.meta.env.MODE === 'production' ? '/kahootgpt' : '';
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter basename={basename}>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 )
